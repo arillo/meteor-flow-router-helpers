ab915248b06464624fda4580ce252dcfacc3556d
DM <dm@arillo.net>
2015-06-09 10:37:33 +0200

    bump version for iron-router pathFor syntax

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/arillo/meteor-flow-router-helpers.git',
   name: 'arillo:flow-router-helpers',
   summary: 'Template helpers for flow-router, pathFor, urlFor, isSubReady and zimme:active-route helpers',
-  version: '0.2.0'
+  version: '0.2.1'
 });
 
 Package.onUse(function(api) {
