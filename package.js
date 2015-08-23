42e60cbf23f68a24d9344abece90b19e41281221
Serkan Durusoy <serkandurusoy@users.noreply.github.com>
2015-08-23 14:51:37 +0300

    Update version information (as semver minor patch)

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/arillo/meteor-flow-router-helpers.git',
   name: 'arillo:flow-router-helpers',
   summary: 'Template helpers for flow-router',
-  version: '0.4.4'
+  version: '0.4.4_1'
 });
 
 Package.onUse(function(api) {
